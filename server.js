412c5ca5bdfd46a9ac739f07ea48cb71f0fc3f80
Jaishree Bala <jaishree1bala@gmail.com>
2020-11-04 20:05:30 -0500

    Established connection with mongoose, added colors

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const connectDB = require("./config/db")
+const colors = require("colors");
 dotenv.config({ path: './config/config.env' });
-
 const app = express();
-// Bring in writtenworks router
 
+// Connect To Database
+connectDB();
+
+// Bring in writtenworks router
 const writtenWorks = require('./router/writtenWorks')
 app.use('/api/v1/writtenWork', writtenWorks);
 
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server up and running on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server up and running on port ${PORT}`.blue.bold));
+
+process.on("unhandledRejection", (err, response) => {
+    console.log(`Error: ${err}`.red.underline);
+    server.close(() => {
+        process.exit(1);
+    })
+})
