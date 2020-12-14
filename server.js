3032905d4405f8b436d907e7cd182c9037d4e2dd
Jaishree Bala <jaishree1bala@gmail.com>
2020-12-13 20:57:08 -0500

    Deployment bugs

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, './client/build')));// Handle React routing, return all requests to React app
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, './client/build', './index.html'));
     });
 }
 console.log(path.join(__dirname, './client/build'))
